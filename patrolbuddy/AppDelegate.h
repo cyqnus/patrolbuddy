f3fed6f91f24691af00d99f9cae81d8123726d28
billypt <billy.pt@hotmail.com>
2013-12-26 00:22:03 -0800

    Initial Commit

---
+++ patrolbuddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  patrolbuddy
+//
+//  Created by Billy Prasetya Tjahjadi on 12/26/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ patrolbuddy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  patrolbuddy
+//
+//  Created by Billy Prasetya Tjahjadi on 12/26/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
